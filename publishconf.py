cb697279aafcef4b052ba4c28535bbb210a5cedc
thomasdeangelis <Tommy@wl-10-190-53-144.wireless.duke.edu>
2013-09-19 16:06:14 -0400

    new

---
+++ publishconf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+import sys
+sys.path.append('.')
+from pelicanconf import *
+
+
+SITEURL = 'http://isaacbythewood.com'
+DELETE_OUTPUT_DIRECTORY = True
+GOOGLE_ANALYTICS = 'UA-35235932-1'
