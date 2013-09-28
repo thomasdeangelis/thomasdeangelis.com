424a76514a2566662c5fc598d0022178fc4a550f
thomasdeangelis <Tommy@wl-10-190-23-216.wireless.duke.edu>
2013-09-28 15:00:42 -0400

    much improved about

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ $(document).ready(function() {
 				<tr>\
 				<td><img src='img/headshot.jpeg' width=100px></td>\
 				<td>I'm a student at Duke University majoring in <span style='color: #d6c3b6'>Economics</span> and <span style='color: #d6c3b6'>Computer Science</span>.\n\
-				I am \n\
-				very interested in <span style='color: #d6c3b6'>programming</span>, <span style='color: #d6c3b6'>economic</span> and <span style='color: #d6c3b6'>business</span>. \n\
-				I also <span style='color: #d6c3b6'>row</span> for Duke and I like to play <span style='color: #d6c3b6'>go (igo)</span>.</td>\
+				I  \n\
+				enjoy <span style='color: #d6c3b6'>programming</span>, <span style='color: #d6c3b6'>ambient music</span> and <span style='color: #d6c3b6'>economic forecasting</span>. \n\
+				I <span style='color: #d6c3b6'>row</span> for Duke and I like to play <span style='color: #d6c3b6'>go</span> and <span style='color: #d6c3b6'>produce music</span> in my free time.</td>\
 				</tr></table>\
 				For more information, type <span style='color: #d6c3b6'>links</span>\
 				<br><br>\
